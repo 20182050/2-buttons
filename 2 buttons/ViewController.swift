6c8ccee584165530035f4cb843e57b06edfaeafe
Cory Johnson <coryjohnson@Corys-MacBook-Pro.local>
2018-05-02 13:22:16 -0400

    Initial Commit

---
+++ 2 buttons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2 buttons
+//
+//  Created by Cory Johnson on 5/2/18.
+//  Copyright © 2018 Cory Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
